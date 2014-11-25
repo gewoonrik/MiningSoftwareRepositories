ba62ec0b0d423de27f09ba0844810756aead5cf9
Rik Nijessen <rik@keefo.nl>
2014-11-25 16:57:14 +0100

    koppeltabel generator geimplementeerd

--- src/main/scala/bazen/App.scala
+++ src/main/scala/bazen/App.scala
@@ -1,15 +1,75 @@
 package bazen
 
+import java.sql.{ResultSet, DriverManager}
+
+import com.mysql.jdbc.MySQLConnection
+
 /**
  * @author ${user.name}
  */
 object App {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
+
+  var tags = scala.collection.mutable.Map[String, Int]()
+
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    // Change to Your Database Config
+    val conn_str = "jdbc:mysql://localhost:3306/stackoverflow?user=&password="
+
+    // Load the driver
+    classOf[com.mysql.jdbc.Driver]
+
+    // Setup the connection
+    val conn = DriverManager.getConnection(conn_str)
+    val conn2 = DriverManager.getConnection(conn_str)
+
+    try {
+      // Configure to be Read Only
+      val statement = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+
+      // Execute Query
+      val rs = statement.executeQuery("SELECT Id, TagName FROM tags")
+      statement.setFetchSize(Integer.MIN_VALUE);
+
+      // Iterate Over ResultSet
+      while (rs.next) {
+        tags += rs.getString("TagName") -> rs.getInt("Id")
+      }
+      statement.close()
+      println("finished loading tags")
+
+      val statement2 = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+      statement2.setFetchSize(Integer.MIN_VALUE);
+      val posts = statement2.executeQuery("SELECT Tags,Id FROM posts")
+
+      val st = conn2.createStatement();
+      var i:Long = 0;
+      while(posts.next()) {
+        if(i%1000 == 0) {
+          println (i)
+        }
+        val strTags = posts.getString("Tags");
+        val postId = posts.getInt("Id")
+        if(strTags != null) {
+          val curTags = splitTags(strTags)
+          val ids = curTags.map(tags(_))
+          ids.foreach(tagId =>{
+            st.executeUpdate("INSERT INTO posttags VALUES (" + postId + "," +tagId+ ")");
+          })
+        }
+        i+=1;
+      }
+      st.close()
+      statement2.close()
+
+
+    }
+    finally {
+      conn.close
+    }
+  }
+
+  def splitTags(tags : String) : Seq[String] = {
+    tags.filterNot(_ == '<').split('>')
   }
 
 }
