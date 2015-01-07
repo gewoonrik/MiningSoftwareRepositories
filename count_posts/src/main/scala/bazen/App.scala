007e473ff9b4b74eb71db3283c71b70979640624
Rik Nijessen <rik@keefo.nl>
2015-01-07 11:17:17 +0100

    calculated posts_per_version_total

--- count_posts/src/main/scala/bazen/App.scala
+++ count_posts/src/main/scala/bazen/App.scala
@@ -5,6 +5,8 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.{Paths, Files}
 import java.sql.{ResultSet, DriverManager}
 
+import scala.util.parsing.json.{JSONArray, JSONObject, JSON}
+
 
 /**
  * @author ${user.name}
@@ -25,19 +27,34 @@ object App {
     val conn2 = DriverManager.getConnection(conn_str)
 
     try {
+      val input = scala.io.Source.fromFile("..\\data\\bootstrap-versions.csv").getLines().mkString.split(",")
+      val versions = input.map(v => v.stripPrefix("v"))
+
+
       // Configure to be Read Only
       val statement = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
 
       // Execute Query
-      val rs = statement.executeQuery("SELECT COUNT(*) as count, CAST(posts.CreationDate AS DATE) as dag FROM posts INNER JOIN posttags ON posts.id = posttags.PostId WHERE posttags.TagId = 72270 GROUP BY dag")
+      val rs = statement.executeQuery("SELECT Title, Body FROM posts INNER JOIN posttags ON posts.id = posttags.PostId WHERE posttags.TagId = 72270")
 
       // Iterate Over ResultSet
-      println("starting writing")
-      val writer = new PrintWriter(new File("..\\results\\posts_per_day_total.txt" ))
+
+      val numberOfPostsPerVersion = scala.collection.mutable.Map[String, Int]()
       while (rs.next) {
-        println(rs.getDate("dag")+","+rs.getInt("count"))
-        writer.write(rs.getDate("dag")+","+rs.getInt("count")+"\n")
+        val title = rs.getString("Title")
+        val body = rs.getString("Body")
+        versions.foreach(v => {
+          if(title.contains(v) || body.contains(v)) {
+            val postCount = numberOfPostsPerVersion.getOrElseUpdate(v, 0)
+            numberOfPostsPerVersion.put(v, postCount+1)
+          }
+        })
       }
+      println("starting writing")
+      val writer = new PrintWriter(new File("..\\results\\posts_per_version_total.txt" ))
+      numberOfPostsPerVersion.foreach((tuple: (String, Int)) =>{
+        writer.write(tuple._1+","+tuple._2+"\n")
+      })
       writer.close()
 
       statement.close()
