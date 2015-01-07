6892ba0bf0fe9551e52686aca602d1efc014a55c
Erwin van Eyk <Erwin_dso@hotmail.com>
2015-01-07 10:29:22 +0100

    Added R-script for post/day-plot

---
+++ numberofposts.r
@@ -0,0 +1,10 @@
+# Load data
+data <- read.csv2(file="C:\\Users\\Erwin\\Documents\\GitHub\\MiningSoftwareRepositories\\results\\posts_per_day_total.txt", sep=",", dec=".", header=FALSE, colClasses=c("Date",NA))
+data[[1]] <- as.Date(data[[1]])
+
+weekends <-subset(data, (weekdays(V1) %in% c('zaterdag','zondag')))
+weekdays <-subset(data, !(weekdays(V1) %in% c('zaterdag','zondag')))
+
+# plot
+plot(weekdays, type="l", col="blue", main="# of posts about twitter-bootstrap", xlab="date", ylab="# of posts")
+lines(weekends, col="red")
