dd46aadc983e9b68d12659084c79f52fb97ed62f
Erwin van Eyk <Erwin_dso@hotmail.com>
2015-01-07 10:45:14 +0100

    Added R-script for k-means clustering

---
+++ clusteringexample.r
@@ -0,0 +1,11 @@
+require(graphics)
+
+# a 2-dimensional example
+x <- rbind(matrix(rnorm(100, sd = 0.3), ncol = 1),
+           matrix(rnorm(100, mean = 1, sd = 0.3), ncol = 2))
+colnames(x) <- c("x", "y")
+
+# plotting
+(cl <- kmeans(x, 5, nstart = 25))
+plot(x, col = cl$cluster)
+points(cl$centers, col = 1:5, pch = 8)
