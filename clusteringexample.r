d74abaaa8befcdd43f71e6d4c15705cafa12f1fd
erwinvaneyk <erwinvaneyk@gmail.com>
2015-01-20 10:32:56 +0100

    Updated stuff..

--- clusteringexample.r
+++ clusteringexample.r
@@ -1,7 +1,7 @@
 require(graphics)
 
 # a 2-dimensional example
-x <- rbind(matrix(rnorm(100, sd = 0.3), ncol = 1),
+x <- rbind(matrix(rnorm(100, sd = 0.3), ncol = 2),
            matrix(rnorm(100, mean = 1, sd = 0.3), ncol = 2))
 colnames(x) <- c("x", "y")
 
--- clusteringhypothese2test.r
+++ clusteringhypothese2test.r
@@ -1,8 +1,10 @@
 # Load data
 data1 <- read.csv2(file="/home/erwin/Github/MiningSoftwareRepositories/results/average_score_per_version_30_days_after_release_right_2.3.2_release.txt", sep=",", dec=".", header=FALSE)
 data2 <- read.csv2(file="/home/erwin/Github/MiningSoftwareRepositories/results/posts_per_version_total_normalized_30_days.txt", sep=",", dec=".", header=FALSE)
-x <- rbind(data1[[2]],data2[[2]])
-colnames(x) <- c("x", "y")
+data1t = data1[[2]]
+data2t = data2[[2]]
+
+x <- t(rbind(data1t, data2t))
 
 # plotting
 (cl <- kmeans(x, 3, nstart = 25))
